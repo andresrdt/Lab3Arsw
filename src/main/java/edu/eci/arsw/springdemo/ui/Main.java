944781f165a9d13edb768cff5464c4a9a1f56762
andresrdt <andres.rodriguez-de@mail.escuelaing.edu.co>
2019-08-28 08:51:02 -0500

    imagenes

--- src/main/java/edu/eci/arsw/springdemo/ui/Main.java
+++ src/main/java/edu/eci/arsw/springdemo/ui/Main.java
@@ -14,7 +14,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @author hcadavid
  */
 public class Main {
-
     public static void main(String a[]) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
         GrammarChecker gc = ac.getBean(GrammarChecker.class);
